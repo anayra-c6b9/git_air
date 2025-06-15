145c97d1328e1be0b25e9a208d2b004ec38f55a9
anayra-c6b9 <anayra13@proton.me>
2025-06-15 20:20:32 +0900

    Working in UI
    Auth window in progress, have to set up the common window

---
+++ View/auth/index.h
@@ -0,0 +1,9 @@
+#ifndef INDEX_H
+#define INDEX_H
+
+namespace Auth{
+	void display_auth_page();
+	
+}
+
+#endif
---
+++ View/components/app_window/app_window.h
@@ -0,0 +1,9 @@
+using uint = unsigned int;
+
+namespace AppComponent{
+    class AppWindow{
+        private:
+            uint max_win_width = 100;
+            uint menu_bar_height = 6;
+    };
+}
