f116457762b9c3aee0cd7dc6a1094ebb3e6f3d5e
anayra-c6b9 <anayra13@proton.me>
2025-06-17 12:37:31 +0900

    worked on app_window and main
    upcoming: 1. test the window resize, 2. create some UI, 3. Add a loading window

--- View/components/app_window/app_window.h
+++ View/components/app_window/app_window.h
@@ -1,9 +1,29 @@
+#ifndef APP_WINDOW_H
+#define APP_WINDOW_H
+
 using uint = unsigned int;
 
 namespace AppComponent{
-    class AppWindow{
-        private:
-            uint max_win_width = 100;
-            uint menu_bar_height = 6;
-    };
+    	class AppWindow{
+        	private:
+            		uint max_win_width;
+			uint max_win_height;
+            		uint menu_bar_height;
+		public:
+			AppWindow(){
+				menu_bar_height = 3;
+				max_win_width = 78;
+				max_win_height = 23;
+			}
+			// functions to change window dimensions
+			// when window is resized
+			void setAppWinWidth(int width);
+			void setAppWinHeight(int height);
+			uint getAppWinWidth();
+			uint getAppWinWidth();
+			uint getMenuBarHeight();
+			void setWindowSize();	
+    	};
 }
+
+#endif
